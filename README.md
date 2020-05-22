312b93ed822f3d031621d9b91da8803cd7cceb71
Alexandre Mély <alexandre.mely@gmail.com>
2020-05-22 11:57:20 +0200

    option to delete

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 # Check Eval
 
-lightweight php script to quickly check if there infected php file infected by malicious code such as eval.
+lightweight php script to search & remove php files infected by malicious code such as eval.
 Script is searching recursively across sub-directories from folder where it is placed.
-Outputs list of potentially infected files if any along with 301 http error code.
-Best used with cron.
+Outputs list of potentially infected files with option to select which one to delete.
+Exits with 301 http error code is infected file found for use with cron.
 
 ```shell
 $> curl -fsS "https://myside.com/check_eval.php" && echo "no infected files found" || echo "infected files found"
 ```
-
-### License
-
-[![License-shield]](LICENSE.md)
--- check_eval.php
+++ check_eval.php
@@ -7,25 +7,66 @@ error_reporting(E_ALL); // report all errors
 // config
 $root = getcwd();
 $extensions = array ( 'php' );
-$regexpr = "/([^-_a-z]eval\s*\(|@"."include)/i"; // @"."include is to avoid matching this php file
-$ignore = array (); # comma seperated array of filepath to ignore (false positives)
+$regexpr = '/([^-_a-z]eval\s*\(|@'.'include)/i'; // @'.'include is to avoid matching this php file
+$ignore = array (
+);
 
-$infected_files = array ();
-$iterator = new RecursiveDirectoryIterator($root);
-foreach(new RecursiveIteratorIterator($iterator) as $filepath)
-{
-    $tmp = explode('.', $filepath);
-    if (in_array(strtolower(array_pop($tmp)), $extensions)) {
-        if (preg_match($regexpr, file_get_contents($filepath), $name)) {
-            if (!in_array ($filepath, $ignore)) {
-                array_push ($infected_files, $filepath);
+session_start();
+//var_dump ($_POST);
+//var_dump ($_SESSION);
+
+if (isset($_POST['actions'])) {
+    foreach ($_POST["actions"] as $id => $action) {
+        $filepath = $_SESSION['infected_files'][$id]['filepath'];
+        switch ($action) {
+            case 'delete':
+                if (is_file ($filepath)) {
+                    unlink($filepath);
+                    echo 'deleted '.$filepath.'<br />';
+                } else {
+                    echo 'is not file '.$filepath.'<br />';
+                }
+                break;
+            
+            default:
+                echo 'kept '.$filepath.'<br />';
+                break;
+        }
+    }
+} else {
+    $_SESSION['infected_files']=array ();
+    $iterator = new RecursiveDirectoryIterator($root);
+    foreach(new RecursiveIteratorIterator($iterator) as $filepath)
+    {
+        $filepath=(string)$filepath;
+        $tmp = explode('.', $filepath);
+        if (in_array(strtolower(array_pop($tmp)), $extensions)) {
+            $content = file_get_contents($filepath);
+            if (preg_match($regexpr, $content, $name)) {
+                if (!in_array ($filepath, $ignore)) {
+                    array_push ($_SESSION['infected_files'], array (
+                        'filepath' => $filepath,
+                        'content' => $content
+                    ));
+                }
             }
         }
     }
+    $nb_infected_files = count ($_SESSION['infected_files']);
+    if ($nb_infected_files > 0) {
+        http_response_code (400); # trigger error for cron
+        echo 'Found '.$nb_infected_files.' infected files:<br />';
+        echo '<form method="post"><table><thead><tr><th>keep</th><th>delete</th><th>file</th></tr></thead><tbody>';
+        foreach($_SESSION['infected_files'] as $id => $file) {
+            echo '<tr><td><input type="radio" name="actions['.$id.']" value="keep" checked="checked"></td>';
+            echo '<td><input type="radio" name="actions['.$id.']" value="delete"></td>';
+            echo '<td><details><summary>'.$file['filepath'].'</summary>';
+            echo '<pre>'.htmlentities ($file['content']).'</pre></details></td></tr>';
+        }
+        echo '</table><input type="submit"/></form>';
+    } else {
+        echo 'No infected file found<br />';
+    }
 }
-if (count ($infected_files) > 0) {
-    http_response_code (400); # trigger error for cron
-}
-echo "Found ".count ($infected_files)." infected files:<br />";
-echo implode ("<br />", $infected_files);
+echo '<a href=".">Analyse again</a>';
 ?>
