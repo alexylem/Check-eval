befffe3aadd1515e48443ca93f792c6fdcdd0a7e
Alexandre Mély <alexandre.mely@gmail.com>
2020-05-26 15:43:47 +0200

    hashed password protected

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Check Eval
 
-lightweight php script to search & remove php files infected by malicious code such as eval.
+lightweight password-protected php script to detect & clean-up php files infected by malicious code such as eval.
 Script is searching recursively across sub-directories from folder where it is placed.
 Outputs list of potentially infected files with option to select which one to delete or ignore.
-Exits with 301 http error code is infected file found for use with cron.
+Exits with 301 http error code if infected file found for use with cron.
 
 ```shell
 $> curl -fsS "https://myside.com/check_eval.php" && echo "no infected files found" || echo "infected files found"
--- check_eval.php
+++ check_eval.php
@@ -5,18 +5,36 @@ ini_set('display_startup_errors', 1); // report php startup errors
 error_reporting(E_ALL); // report all errors
 
 // config
+$password = '#############'; // generate password_hash here: https://phppasswordhash.com/
 $root = getcwd();
 $extensions = array ( 'php' );
-$regexpr = '/([^-_a-z]eval\s*\(|@'.'include)/i'; // @'.'include is to avoid matching this php file
+$regexpr = '/^.*([^-_a-z]eval\s*\(|@'.'include).*$/im'; // @'.'include is to avoid matching this php file
+
+// init
 $ignores = array ();
 if (file_exists ('ignores.json')) {
     $ignores = json_decode(file_get_contents ('ignores.json'));
 }
 session_start();
-//var_dump ($_POST);
-//var_dump ($_SESSION);
 
-if (isset($_POST['actions'])) {
+// start
+if (isset ($_POST['view'])) {
+    if (isset ($_SESSION['authenticated']) || password_verify ($_POST['password'], $password)) {
+        $_SESSION['authenticated']=1;
+        echo '<form method="post"><table><thead><tr><th>skip</th><th>ignore</th><th>delete</th><th>file</th></tr></thead><tbody>';
+        foreach($_SESSION['infected_files'] as $id => $file) {
+            echo '<tr><td><input type="radio" name="actions['.$id.']" value="skip" checked="checked"></td>';
+            echo '<td><input type="radio" name="actions['.$id.']" value="ignore"></td>';
+            echo '<td><input type="radio" name="actions['.$id.']" value="delete"></td>';
+            echo '<td><details><summary>'.$file['filepath'].'<br /><pre>'.$file['match'].'</pre></summary>';
+            echo '<pre>'.htmlentities ($file['content']).'</pre></details></td></tr>';
+        }
+        echo '</table><input type="submit" value="Execute" /></form>';
+    } else {
+        echo '<font color="red">wrong password</font>';
+        echo '<form method="post"><input type="hidden" name="view" value="1" /><input type="password" name="password" placeholder="Password" /><input type="submit" value="View" /></form>';
+    }
+} else if (isset($_POST['actions']) && isset($_SESSION['infected_files'])) {
     foreach ($_POST["actions"] as $id => $action) {
         $filepath = $_SESSION['infected_files'][$id]['filepath'];
         switch ($action) {
@@ -33,7 +51,8 @@ if (isset($_POST['actions'])) {
                 break;
         }
     }
-    file_put_contents('ignores.json', json_encode($ignores));
+    file_put_contents('ignores.json', json_encode($ignores, JSON_PRETTY_PRINT));
+    echo '<a href="'.basename(__FILE__).'">Analyse again</a>';
 } else {
     $_SESSION['infected_files']=array ();
     $iterator = new RecursiveDirectoryIterator($root);
@@ -43,10 +62,11 @@ if (isset($_POST['actions'])) {
         $tmp = explode('.', $filepath);
         if (in_array(strtolower(array_pop($tmp)), $extensions)) {
             $content = file_get_contents($filepath);
-            if (preg_match($regexpr, $content, $name)) {
+            if (preg_match($regexpr, $content, $match)) {
                 if (!in_array ($filepath, $ignores)) {
                     array_push ($_SESSION['infected_files'], array (
                         'filepath' => $filepath,
+                        'match' => $match[0],
                         'content' => $content
                     ));
                 }
@@ -56,19 +76,15 @@ if (isset($_POST['actions'])) {
     $nb_infected_files = count ($_SESSION['infected_files']);
     if ($nb_infected_files > 0) {
         http_response_code (400); # trigger error for cron
-        echo 'Found '.$nb_infected_files.' infected files:<br />';
-        echo '<form method="post"><table><thead><tr><th>skip</th><th>ignore</th><th>delete</th><th>file</th></tr></thead><tbody>';
-        foreach($_SESSION['infected_files'] as $id => $file) {
-            echo '<tr><td><input type="radio" name="actions['.$id.']" value="skip" checked="checked"></td>';
-            echo '<td><input type="radio" name="actions['.$id.']" value="ignore"></td>';
-            echo '<td><input type="radio" name="actions['.$id.']" value="delete"></td>';
-            echo '<td><details><summary>'.$file['filepath'].'</summary>';
-            echo '<pre>'.htmlentities ($file['content']).'</pre></details></td></tr>';
+        echo 'Found '.$nb_infected_files.' infected files<br />';
+        echo '<form method="post">';
+        echo '<input type="hidden" name="view" value="1" />';
+        if (!isset ($_SESSION['authenticated'])) {
+            echo '<input type="password" name="password" placeholder="Password" />';
         }
-        echo '</table><input type="submit"/></form>';
+        echo '<input type="submit" value="View" /></form>';
     } else {
         echo 'No infected file found<br />';
     }
 }
-echo '<a href="'.basename(__FILE__).'">Analyse again</a>';
 ?>
